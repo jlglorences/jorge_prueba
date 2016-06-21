5cefa7991c8a7d42db5d40ed28212da45dbf71cf
jlglorences <jlglorences@gmail.com>
2016-06-21 12:07:12 +0200

    Revert "sdgdf"
    This reverts commit 2714e3134847a1e3f69975e6ddf1c6d68eecc421.
    
    	modified:   autoload.php
    	modified:   index.php

--- autoload.php
+++ autoload.php
@@ -7,5 +7,3 @@ require_once __DIR__ . '/app/KernelLoader.php';
 // Spoon is not autoloaded via Composer but uses its own old skool autoloader
 set_include_path(__DIR__ . '/vendor/spoon/library' . PATH_SEPARATOR . get_include_path());
 require_once 'spoon/spoon.php';
-
-sdfgsdgfsd
